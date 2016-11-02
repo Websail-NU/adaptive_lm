4a86979c24c2dd23cc7887a67cedad9081ac06da
Thanapon Noraset <nor.thanapon@u.northwestern.edu>
2016-11-02 13:12:56 -0500

    added BOW vocab

--- data/get_cbtest.sh
+++ data/get_cbtest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "[1/3] Downloading data..."
 wget http://www.thespermwhale.com/jaseweston/babi/CBTest.tgz
-tar -xvf CBTest.tgz
+echo "[2/3] Extracting files..."
+tar --warning=no-unknown-keyword -xf CBTest.tgz
 rm CBTest.tgz
 mkdir CBTest/preprocess
-python ../preprocess/preprocess_cbtest.py CBTest/
+echo "[3/3] Preprocessing text files..."
+python ../preprocess/preprocess_cbtest.py CBTest/ stopwords.txt
