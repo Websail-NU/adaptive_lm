704007e95afdc7a0ed87bd986f727ee4a53346b5
Thanapon Noraset <nor.thanapon@u.northwestern.edu>
2017-01-24 15:48:47 -0600

    minor change to file location

--- exp_scripts/run_ptb100.sh
+++ exp_scripts/run_ptb100.sh
@@ -7,21 +7,11 @@ function rm_if_exist {
   fi
 }
 
-SHARED_EMB_VOCAB="data/common_defs_v1.2/shortlist/common_defs_ptb_shortlist.txt"
+SHARED_EMB_VOCAB="data/ptb-100/common_defs_ptb_shortlist.txt"
 MODEL_OPTIONS="--state_size 300 --emb_size 300  --num_layers 2 --sen_independent --keep_prob 0.5 --emb_keep_prob 0.5"
 TRAIN_OPTIONS="--lr_decay_wait 2 --lr_decay_factor 0.6 --min_learning_rate 0.05 --max_grad_norm 5 --num_steps 10  --learning_rate 1.0 --debug"
 
-EXP_DIR="experiments/ptb-100/0-reset/"
-rm_if_exist $EXP_DIR
-mkdir -p $EXP_DIR
-ipython -- train.py $MODEL_OPTIONS $TRAIN_OPTIONS --log_file_path $EXP_DIR"training.log" --output_dir $EXP_DIR --data_dir data/ptb/preprocess/
-
-EXP_DIR="experiments/ptb-100/0-reset-shared/"
-rm_if_exist $EXP_DIR
-mkdir -p $EXP_DIR
-ipython -- train.py $MODEL_OPTIONS $TRAIN_OPTIONS --log_file_path $EXP_DIR"training.log" --output_dir $EXP_DIR --data_dir data/ptb/preprocess/ --shared_emb_vocab $SHARED_EMB_VOCAB --shared_emb_lm_logit
-
-DROP_RATES="100 80 60 40 20"
+DROP_RATES="0 20 40 60 80 100"
 
 for RATE in $DROP_RATES; do
   EXP_DIR="experiments/ptb-100/"$RATE"-reset/"
